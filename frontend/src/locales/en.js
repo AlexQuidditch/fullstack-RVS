479ad463dd47639f4f7d7e9306475a6468080f1c
AlexQuidditch <alexquidditch@gmail.com>
2018-03-10 22:41:08 +0300

    added locales

---
+++ frontend/src/locales/en.js
@@ -0,0 +1,15 @@
+export default {
+  main: {
+    title: 'Remote Viewing System - pre-Alpha'
+  },
+  message: {
+    hello: 'hello world'
+  },
+  register: {
+    name: 'First name',
+    email: 'E-mail',
+    password: 'Password',
+    confirm: 'Confirm password',
+    role: 'Select role'
+  }
+};
---
+++ frontend/src/locales/ru.js
@@ -0,0 +1,27 @@
+export default {
+  main: {
+    title: 'Remote Viewing System - pre-Alpha'
+  },
+  message: {
+    hello: 'Привет мир!'
+  },
+  register: {
+    name: 'Имя',
+    email: 'Электронная почта',
+    password: 'Пароль',
+    confirm: 'Повторите пароль',
+    role: 'Выберите роль',
+    validation: {
+      name: {
+        required: 'Требуется имя'
+      }
+    }
+  },
+  protocols: {
+    ui: {
+      create: 'Создать',
+      view: 'Просмотреть',
+      complete: 'Оценить'
+    }
+  }
+};
