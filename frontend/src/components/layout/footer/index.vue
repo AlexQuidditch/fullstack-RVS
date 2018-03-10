a0364016f3f4eeed22fe612ad152b2e0f9ca06de
AlexQuidditch <alexquidditch@gmail.com>
2018-03-10 22:35:22 +0300

    added footer

---
+++ frontend/src/components/layout/footer/index.vue
@@ -0,0 +1,34 @@
+<template lang="html">
+  <v-footer app :height="56"
+            class="footer px-3">
+    <v-btn to="/">Главная</v-btn>
+  </v-footer>
+</template>
+
+<script>
+
+  export default {
+    name: 'Page-Footer',
+    data: () => ({
+      fixed: false,
+      loading: false
+    }),
+    computed: {
+      isDark: {
+        get () { return this.$store.state.UI.global.isDark },
+        set (value) { return this.$store.commit( 'setGlobalDark' , value ) }
+      }
+    }
+  };
+
+</script>
+
+<style lang="scss">
+
+  .footer {
+    display: flex;
+    justify-content: center;
+    height: 60px;
+  }
+
+</style>
