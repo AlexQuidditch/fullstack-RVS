2c47c250a039806eba889cc1a34fd752c3b1a2b4
AlexQuidditch <alexquidditch@gmail.com>
2018-03-10 22:35:31 +0300

    added header

---
+++ frontend/src/components/layout/header/index.vue
@@ -0,0 +1,38 @@
+<template lang="html">
+  <v-toolbar app :clipped-left="clipped">
+    <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+    <v-toolbar-title>{{ $t('main.title') }}</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn icon @click.stop="rightDrawer = !rightDrawer">
+      <v-icon>menu</v-icon>
+    </v-btn>
+  </v-toolbar>
+</template>
+
+<script>
+
+  export default {
+    name: 'Page-Header',
+    computed: {
+      rightDrawer: {
+        get () { return this.$store.state.UI.toolbar.rightDrawer },
+        set (value) { return this.$store.commit( 'setToolbarRightDrawer' , value ) }
+      },
+      drawer: {
+        get () { return this.$store.state.UI.toolbar.drawer },
+        set (value) { return this.$store.commit( 'setToolbarDrawer' , value ) }
+      },
+      clipped: {
+        get () { return this.$store.state.UI.toolbar.clipped },
+        set (value) { return this.$store.commit( 'setToolbarClip' , value ) }
+      }
+    }
+  };
+
+</script>
+
+<style lang="scss">
+
+  .Page-Header {}
+
+</style>
